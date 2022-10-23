5f7bfe55152d0eac83bbeb6dd2ebd6c224f57e6c
Zohid7717 <101669736+Zohid7717@users.noreply.github.com>
2022-10-23 23:41:43 +0500

    закончил секции header и main

--- js/script.js
+++ js/script.js
@@ -5,4 +5,7 @@ $(document).ready(function () {
   $('.header__burger').click(function (event) {
     $('.header__burger,.header__title,.burger__block,.header__services').toggleClass('active');
   });
+  $('.service-menu__burger').click(function (event) {
+    $('.service-menu__burger,.service-menu__list,.service-menu__burger-link').toggleClass('active');
+  });
 });
