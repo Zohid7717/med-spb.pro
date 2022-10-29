4a3bded8a02647bae98b4448e60ee03ef30fb8ca
Zohid7717 <101669736+Zohid7717@users.noreply.github.com>
2022-10-29 06:37:45 +0500

    добавления файла faq.js

---
+++ js/faq.js
@@ -0,0 +1,3 @@
+document.querySelector('.faq-dropdown__button').addEventListener('click', function () {
+  document.querySelector('.faq-dropdown__text').classList.toggle('faq-dropdown__text-visible');
+});
