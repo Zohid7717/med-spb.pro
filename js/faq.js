d5560ea2b0292973f5cecd751ce7720faf7e9177
Zohid7717 <101669736+Zohid7717@users.noreply.github.com>
2022-10-29 23:44:06 +0500

    доработал faq main

--- js/faq.js
+++ js/faq.js
@@ -1,3 +1,20 @@
-document.querySelector('.faq-dropdown__button').addEventListener('click', function () {
-  document.querySelector('.faq-dropdown__text').classList.toggle('faq-dropdown__text-visible');
+document.querySelectorAll('.faq-dropdown__container').forEach(function (faqDropdownWrapper) {
+  
+  const faqDropdownButton = faqDropdownWrapper.querySelector('.faq-dropdown__button');
+  const faqDropdownText = faqDropdownWrapper.querySelector('.faq-dropdown__text');
+  const faqDropdownButtonIcon = faqDropdownWrapper.querySelector('.faq-dropdown__button-icon');
+  faqDropdownButton.addEventListener('click', function (e) {
+    e.stopPropagation();
+    faqDropdownText.classList.toggle('faq-dropdown__text-visible');
+    faqDropdownButton.classList.toggle('active');
+    faqDropdownButtonIcon.classList.toggle('active');
+  });
+  document.addEventListener('click', function (e) {
+    if (e.target !== faqDropdownButton) {
+      faqDropdownButton.classList.remove('active');
+      faqDropdownButtonIcon.classList.remove('active');
+      faqDropdownText.classList.remove('faq-dropdown__text-visible');
+    }
+  })
 });
+
